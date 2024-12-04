0dcc54ecd63a8da16c3f83d63e1e7ba4f8eb9539
himel1625 <himelmia1625@gmail.com>
2024-12-04 18:37:41 +0600

    implement search option

--- src/UI/Navbar.jsx
+++ src/UI/Navbar.jsx
@@ -18,21 +18,22 @@ const Navbar = () => {
           className={({ isActive }) =>
             `font-bold ${isActive ? 'text-green-600' : 'hover:text-green-600'}`
           }
-        > All-Equipment</NavLink>
-       
+        >
+          All-Equipment
+        </NavLink>
       </div>
     </>
   );
 
   return (
     <>
-      <div className="navbar h-20  sticky top-0 z-10 backdrop-filter backdrop-blur-lg ">
+      <div className="navbar h-20 sticky top-0 z-10 backdrop-filter backdrop-blur-lg px-4">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-12 w-12"
+                className="h-10 w-10  "
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -54,28 +55,37 @@ const Navbar = () => {
           </div>
 
           <NavLink to="/">
-            <h1 className=" text-2xl font-bold">Green Sports</h1>
+            <h1 className="text-3xl font-bold text-green-400 drop-shadow-[0_2px_4px_rgba(56,161,105,0.75)]">
+              Green Sports
+            </h1>
           </NavLink>
         </div>
-        <div className="navbar-end">
+
+        <div className="navbar-end flex items-center space-x-4">
+          <div className="relative lg:block hidden">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="input input-bordered w-[200px] h-[40px] rounded-full focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+
           <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">{links}</ul>
           </div>
 
-          <div className="mx-4">
-            <div>
-              <img
-                className="w-12 h-12 rounded-full cursor-pointer"
-                src={
-                  // user?.photoURL ||
-                  'https://cdn-icons-png.flaticon.com/512/8847/8847419.png'
-                }
-                alt="User Avatar"
-              />
-            </div>
-          </div>
-          <div>
-            <NavLink to="/Login">Login</NavLink>
+          <div className="flex items-center space-x-4">
+            <img
+              className="w-12 h-12 rounded-full cursor-pointer"
+              src="https://cdn-icons-png.flaticon.com/512/8847/8847419.png"
+              alt="User Avatar"
+            />
+            <NavLink
+              to="/Login"
+              className="font-bold text-green-600 hover:text-green-800"
+            >
+              Login
+            </NavLink>
           </div>
         </div>
       </div>
